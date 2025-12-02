55cddcf41e02189cd0f6599ff7f6dcba88ab1009
Thiago Cipitelli <cipitelliprata@gmail.com>
2025-12-02 17:57:27 -0300

    day-1

---
+++ day1/day1.py
@@ -0,0 +1,20 @@
+with open("input.txt", "r") as file:
+    lines = file.read().splitlines()
+
+dial = 50
+cont = 0
+for line in lines:
+    rotation_direction = line[0].lower()
+    moves = int(line[1:])
+    if rotation_direction == 'l':
+        dial -= moves
+    if rotation_direction == 'r':
+        dial += moves
+    if dial >= 100:
+        dial %= 100
+    if dial < 0:
+        dial %= 100
+    if dial == 0:
+        cont += 1
+
+print(cont)
